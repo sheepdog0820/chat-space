e814b14334396714e52c744f04a20e113bff2ac6
sheepdog0820 <sheepdgo1919@gmail.com>
2020-03-02 17:32:50 +0900

    ajax-message vol.6 htmlにレスポンスを返す
    エラー
    builderからのJSONになっていない

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,44 @@
 $(function(){
+  function buildHTML(message){
+    if (message.image){
+      var html =
+        `<div class="message">
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+            ${message.user_name}
+            </div>
+            <div class="upper-message__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="lower-message">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+          </div>
+            <img src=${message.image} ></img>
+        </div>`
+        return html;
+    } else {
+      var html =
+       `<div class="message">
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+              ${message.user_name}
+            </div>
+            <div class="upper-message__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="lower-message">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+          </div>
+        </div>`
+        return html;
+    };
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault()
     var formData = new FormData(this);
@@ -11,5 +51,8 @@ $(function(){
       processData: false,
       contentType: false
     })
+      .done(function(data){
+        var html = buildHTML(data);
+      })
   });
 });
