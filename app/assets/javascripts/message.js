63a47d8c84355274cf6bda24d48d299d51fdfa33
sheepdog0820 <sheepdgo1919@gmail.com>
2020-03-02 20:51:35 +0900

    ajax-message vol.7 エラー解消
    セミコンを修正
    appendの対象をmessagesからmain-bar__messagesに変更

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,11 +1,11 @@
-$(function(){
+$(function(){ 
   function buildHTML(message){
-    if (message.image){
+    if ( message.image ) {
       var html =
-        `<div class="message">
+       `<div class="message">
           <div class="upper-message">
             <div class="upper-message__user-name">
-            ${message.user_name}
+              ${message.user_name}
             </div>
             <div class="upper-message__date">
               ${message.created_at}
@@ -16,9 +16,9 @@ $(function(){
               ${message.content}
             </p>
           </div>
-            <img src=${message.image} ></img>
+          <img src=${message.image} >
         </div>`
-        return html;
+      return html;
     } else {
       var html =
        `<div class="message">
@@ -36,23 +36,25 @@ $(function(){
             </p>
           </div>
         </div>`
-        return html;
+      return html;
     };
   }
-  $('#new_message').on('submit', function(e){
-    e.preventDefault()
-    var formData = new FormData(this);
-    var url = $(this).attr('action');
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
+$('#new_message').on('submit', function(e){
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action')
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+  })
+    .done(function(data){
+      var html = buildHTML(data);
+      $(".main-bar__messages").append(html);
+      $('form')[0].reset();
     })
-      .done(function(data){
-        var html = buildHTML(data);
-      })
-  });
+  })
 });
