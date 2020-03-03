6d8ffdcfe36ca0a195b3362c7c3b3a460599a0db
sheepdog0820 <sheepdgo1919@gmail.com>
2020-03-03 17:31:07 +0900

    Incremental search vol.9 メンバー追加時の改行を修正
    改行のエラーを修正

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -20,12 +20,11 @@ $(function(){
       $("#user-search-result").append(html);
   }
 
-  function addDeleteUser(name, id){
-    let html =`
-    <div class="chat-group-user cleaffix" id="${id}">
-      <p class="caht-group-user__name">${name}</p>
-      <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn' data-user-id="${id}" data-user-name="${name}">削除
-      </div>
+  function addDeleteUser(name, id) {
+    let html = `
+    <div class="chat-group-user clearfix" id="${id}">
+      <p class="chat-group-user__name">${name}</p>
+      <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">削除</div>
     </div>`;
     $(".js-add-user").append(html);
   }
