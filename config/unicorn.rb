f2e0bd77fdd37095f0dc4ef8bcf1c585bca4f7ba
sheepdog0820 <sheepdgo1919@gmail.com>
2020-03-05 13:33:59 +0900

    ユニコーン

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,11 +1,18 @@
-app_path = File.expand_path('../../../', __FILE__)
+app_path = File.expand_path('../../', __FILE__)
 worker_processes 1
-working_directory "#{app_path}/current"
-listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
-pid "#{app_path}/shared/tmp/pids/unicorn.pid"
-stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
-stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
+#アプリケーションの設置されているディレクトリを指定
+working_directory app_path
+#Unicornの起動に必要なファイルの設置場所を指定
+pid "#{app_path}/tmp/pids/unicorn.pid"
+#ポート番号を指定
+listen 3000
+#エラーのログを記録するファイルを指定
+stderr_path "#{app_path}/log/unicorn.stderr.log"
+#通常のログを記録するファイルを指定
+stdout_path "#{app_path}/log/unicorn.stdout.log"
+#Railsアプリケーションの応答を待つ上限時間を設定
 timeout 60
+#以下は応用的な設定なので説明は割愛
 preload_app true
 GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
 check_client_connection false
@@ -26,6 +33,7 @@ before_fork do |server, worker|
     end
   end
 end
+
 after_fork do |_server, _worker|
   defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
 end
