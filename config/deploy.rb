10e3ba40fe4927b3c1ce5819a2eaa8f86d905133
sheepdog0820 <sheepdgo1919@gmail.com>
2020-03-05 14:33:27 +0900

    デプロイ vol.2

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,18 @@
-lock "~> 3.12.0"
+# config valid only for current version of Capistrano
+# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
+lock '3.12.0'
 
-set :application, "chat-space"
-set :repo_url, "git@github.com:sheepdog/chat-space.git"
+# Capistranoのログの表示に利用する
+set :application, 'chat-space'
+
+# どのリポジトリからアプリをpullするかを指定する
+set :repo_url,  'git@github.com:sheepdog0820/chat-space.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, 2.5.1
+set :rbenv_ruby, '2.5.1'
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
