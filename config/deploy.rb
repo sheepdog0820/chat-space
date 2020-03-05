1cd341f685598e17b3dc9d505401373ae510a936
sheepdog0820 <sheepdgo1919@gmail.com>
2020-03-05 21:04:27 +0900

    画面アップロードvol.4

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,7 @@
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
 lock '3.12.0'
 
+set :linked_files, %w{ config/secrets.yml }
 # Capistranoのログの表示に利用する
 set :application, 'chat-space'
 
@@ -25,7 +26,7 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-set :linked_files, %w{ config/secrets.yml }
+
 
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
